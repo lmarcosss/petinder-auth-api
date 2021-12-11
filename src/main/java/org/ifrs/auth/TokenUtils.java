4278bdcbcb7298474ce87526a260fe7ccb4f44a6
Francisco Wolfarth Mossi <francisco.mossi@cwi.com.br>
2021-12-11 17:51:35 -0300

    removing commented code

--- src/main/java/org/ifrs/auth/TokenUtils.java
+++ src/main/java/org/ifrs/auth/TokenUtils.java
@@ -3,11 +3,9 @@ package org.ifrs.auth;
 import java.util.Arrays;
 import java.util.HashSet;
 import io.smallrye.jwt.build.Jwt;
-import org.eclipse.microprofile.config.ConfigProvider;
 
 public class TokenUtils {
     public static String generateToken(String login, Long userId) {
-        // String secretKey = ConfigProvider.getConfig().getValue("petinder.secret", String.class);
         return Jwt
             .upn(login) 
             .groups(new HashSet<>(Arrays.asList("User"))) 
